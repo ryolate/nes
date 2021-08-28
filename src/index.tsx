fffd9bc7c7313f92cb976c7a924556b94483ae46
ryo <whatever@example.com>
2021-08-29 07:00:11 +0900

    tmp

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+    HashRouter as Router,
+    NavLink, Route, Switch
+} from 'react-router-dom'
 import * as NESUI from './ui/main'
 
+const CIApp = () => {
+    return <div>Hello CI!</div>
+}
+
+function App() {
+    return (
+        <Router>
+            <nav style={{
+                display: "flex",
+                gap: 10,
+            }}>
+                <NavLink exact to="/">nes</NavLink>
+                <NavLink to="/ci">ci</NavLink>
+            </nav>
+            <div>
+                <Switch>
+                    <Route path="/ci"><CIApp /></Route>
+                    <Route path="/"><NESUI.App /></Route>
+                </Switch>
+            </div>
+        </Router >
+    )
+}
+
 ReactDOM.render(
-    <NESUI.App />,
+    <App />,
     document.getElementById('app'),
 )
