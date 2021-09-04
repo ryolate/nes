8c8583e615d8649ad1650fa2c87629fb03fa6a09
ryo <whatever@example.com>
2021-09-04 19:32:46 +0900

    speed up attempt wip

--- src/testing/bench.ts
+++ src/testing/bench.ts
@@ -1,11 +1,11 @@
-import fs from 'fs'
+import * as fs from 'node:fs'
 import { NES } from '../nes/nes'
 import { performance } from 'node:perf_hooks'
 
 // 15.67s on 9c791512ba29d09927aef2fbb5cc35661b3a249e
 // run with `npx ts-node src/testing/bench.ts`.
 const filepath = 'testdata/secret/SUPER_MARIO_BROS.NES'
-const frameCount = 1200
+const frameCount = 1200 // 20 seconds.
 
 const data = fs.readFileSync(filepath)
 const nes = NES.fromCartridgeData(data)
